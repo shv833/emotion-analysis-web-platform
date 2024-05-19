43ecb35f3d9dd22394fcee30d7bdab9fbb0a30e1
shv833 <shyra.volodymyr.833@gmail.com>
2024-05-19 23:06:14 +0300

    Added favicon

--- backend/backend/urls/api.py
+++ backend/backend/urls/api.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 
 v1 = "api/v1/"
 
 urlpatterns = [
+    path("favicon.ico", RedirectView.as_view(url='images/favicon.ico')),
     path(v1 + "courses/", include("courses.urls")),
     path(v1 + "groups/", include("groups.urls")),
     path(v1 + "users/", include("users.urls")),
